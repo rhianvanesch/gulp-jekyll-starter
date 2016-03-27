1fc85977c1b63cc101b3d77344f7d60e3deb4938
Rhian van Esch <rhian.vanesch@gmail.com>
2016-03-27 20:02:19 +0100

    Update sass task to correct sourcemap generation

--- gulp/tasks/development/sass.js
+++ gulp/tasks/development/sass.js
@@ -2,29 +2,20 @@ var gulp         = require('gulp');
 var plumber      = require('gulp-plumber');
 var browsersync  = require('browser-sync');
 var sass         = require('gulp-ruby-sass');
-var gulpFilter   = require('gulp-filter');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps   = require('gulp-sourcemaps');
-var config       = require('../../config');
+var config       = require('../../config').sass;
 
 // Generate CSS from SCSS and build sourcemaps
 
 gulp.task('sass', function() {
-  var sassConfig = config.sass.options;
-
-  sassConfig.onError = browsersync.notify;
-
-  // Don’t write sourcemaps of sourcemaps
-  var filter = gulpFilter(['*.css', '!*.map'], { restore: true });
-
+  config.options.onError = browsersync.notify;
   browsersync.notify('Compiling Sass');
 
-  return sass(config.sass.src, sassConfig)
+  return sass(config.src, config.options)
     .pipe(plumber()) // keeps gulp running if there's a syntax error in a file
     .pipe(sourcemaps.init())
-    .pipe(autoprefixer(config.autoprefixer))
-    .pipe(filter) // Don’t write sourcemaps of sourcemaps
-    .pipe(sourcemaps.write('.', { includeContent: false, sourceRoot: 'app/_assets/scss' }))
-    .pipe(filter.restore) // Restore original files
-    .pipe(gulp.dest(config.sass.dest));
+      .pipe(autoprefixer(config.autoprefixer))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.dest));
 });
